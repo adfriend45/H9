7d9e16809bed81cdd4d5d411977bec133a1adf7f
adfriend45 <adf10@cam.ac.uk>
2014-12-18 10:24:13 +0000

    CAD limit and lim a %age.

--- SOURCE/structures.f90
+++ SOURCE/structures.f90
@@ -19,8 +19,9 @@ USE TREE
 IMPLICIT NONE
 !----------------------------------------------------------------------!
 
-REAL :: iPAR_base_N,fi,Asapwood,Astem,BCR,Afoliage_sum,K,lim,LAI_save
-REAL :: LAI_above_base,ib_mean
+REAL :: iPAR_base_N,fi,Asapwood,Astem,BCR,Afoliage_sum,K,lim
+REAL :: ib_mean_save
+REAL :: LAI_above_base,ib_mean,CL,Vcrown
 
 !----------------------------------------------------------------------!
 ! Compute crown heights and areas based on new Cv's.
@@ -29,9 +30,8 @@ CALL squeeze
 !----------------------------------------------------------------------!
 
 lim = 10.0
-LAI_save = 0.0
-!DO WHILE (lim > 0.01)
-DO WHILE (lim > 1.0)
+ib_mean_save = 0.0
+DO WHILE (lim > 0.01) ! Limit is on fractional change in mean ib.
 
   !-------------------------------------------------------------------!
   ! Compute fPAR, iPAR, and iPAR_base.
@@ -40,7 +40,7 @@ DO WHILE (lim > 1.0)
   !-------------------------------------------------------------------!
 
   !-------------------------------------------------------------------!
-  ! Compute potential crown Afoliages based on iPAR_base.
+  ! Compute potential crown Afoliages based on iPAR_base and Vcrown.
   !-------------------------------------------------------------------!
   DO I = 1, NIND_alive
     KI = LIVING (I)
@@ -48,6 +48,7 @@ DO WHILE (lim > 1.0)
       LAI_above_base = LOG (iPAR_base (KI)) / kext
       Afoliage_want (KI) = Aplot * (LOG (0.03) / kext - &
       &                    LAI_above_base) + Afoliage (KI)
+      !---------------------------------------------------------------!
     END IF
   END DO
 
@@ -57,6 +58,15 @@ DO WHILE (lim > 1.0)
     KI = LIVING (I)
     BCR = BRC_limit * (1.0 - iPAR_base (KI))
     ib (KI) = NINT (FLOAT (ih (KI)) * BCR)
+    !-----------------------------------------------------------------!
+    ! Limit foliage area density to a sensible value given crown
+    ! volume. 10 m^2/m^3 would sensible (i.e. LAI of 10 in 1 m tall
+    ! grass sward), but to allow for extremes, use 20 m^2/m^3.
+    !-----------------------------------------------------------------!
+    CL = H (KI) - FLOOR (ib (KI) * DZ_CROWN_M)
+    Vcrown = CL * Acrown (KI)
+    Afoliage_want (KI) = MIN (Afoliage_want(KI),20.0*Vcrown)
+    !-----------------------------------------------------------------!
     ib_mean = ib_mean + ib (KI)
   END DO
   ib_mean = ib_mean / FLOAT (NIND_alive)
@@ -66,17 +76,16 @@ DO WHILE (lim > 1.0)
   DO I = 1, NIND_alive
     KI = LIVING (I)
     IF (Acrown (KI) > EPS) THEN
-      ! Need to move this up and make sure Aheart is kept to its initial
-      ! value at the start of each iteration.
       Astem = pi * r (KI) ** 2
-      Aheart (KI) = MIN (Aheart (KI), Astem) ! In case of shrinkage.
+      ! In case of shrinkage.
+      Aheart_new (KI) = MIN (Aheart_new (KI), Astem)
       Asapwood = Afoliage_want (KI) / FASA
-      IF (Asapwood < (Astem - Aheart (KI))) THEN
-        Aheart (KI) = Astem - Asapwood
+      IF (Asapwood < (Astem - Aheart_new (KI))) THEN
+        Aheart_new (KI) = Astem - Asapwood
         Afoliage (KI) = Afoliage_want (KI)
       ELSE
-        Afoliage (KI) = FASA * (Astem - Aheart (KI))
-        if (afoliage (KI) < 0.0) stop
+        Afoliage (KI) = FASA * (Astem - Aheart_new (KI))
+        if (afoliage (KI) < 0.0) stop 'Afoliage < 0 in structures'
       ENDIF
       LAIcrown (KI) = Afoliage (KI) / Acrown (KI)
     ELSE
@@ -86,10 +95,8 @@ DO WHILE (lim > 1.0)
     Afoliage_sum = Afoliage_sum + Afoliage (KI)
   END DO
   LAI = Afoliage_sum / (Aplot + EPS)
-  !lim = ABS (LAI - LAI_save)
-  lim = ABS (ib_mean - LAI_save)
-  !LAI_save = LAI
-  LAI_save = ib_mean
+  lim = ABS (ib_mean - ib_mean_save) / (ib_mean_save + EPS)
+  ib_mean_save = ib_mean
   WRITE (*,*) JYEAR,'LAI = ',LAI,ib_mean
 END DO
 
@@ -103,6 +110,7 @@ DO I = 1, NIND_alive
   ! Foliage biomass                                             (kg[DM})
   !--------------------------------------------------------------------!
   Bfoliage (KI) = Afoliage (KI) / SLA
+  Aheart (KI) = Aheart_new (KI)
   !--------------------------------------------------------------------!
 END DO
 !----------------------------------------------------------------------!
